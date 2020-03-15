2464b93b536eefef55c3fb59bb1382bf3f326dd6
albert507 <albert507@naver.com>
2020-03-15 14:53:38 +0900

    1번 제출

--- Tobigs/Week7/W7_Q1.py
+++ Tobigs/Week7/W7_Q1.py
@@ -1,36 +1,38 @@
 import sys
+from _collections import deque
 
 R, C = map(int, input().split())
 board = []
-min = sys.maxsize
+result = 1
+q = deque()
 
 for i in range(R):
     line = sys.stdin.readline().strip()
     board.append(list(line))
 
-def solve():
-    # 상하좌우 표현
+
+def solve(start_x, start_y):
+    global result
+
+    # 상하좌우 움직임을 표현
     move1 = [-1, 0, 0, 1]
     move2 = [0, 1, -1, 0]
-    stack = []
-    trace = []
 
-    start = [0, 0]
-    stack.append(start)
-    trace.append(board[0][0])
-    while stack:
-        node = stack.pop()
+    q.append([start_x, start_y, board[start_x][start_y]])
+    # 시작점과 시작점의 알파벳을 추가해서 시작!
+
+    while q:
+        node = q.popleft()
         x = node[0]
         y = node[1]
+        root = node[2]
 
         for i in range(4):
-            if 0 <= x + move1[i] < R and 0 <= y + move2[i] < C and board[x + move1[i]][y + move2[i]] not in trace:
-                stack.append([x + move1[i], y + move2[i]])
-                trace.append(board[x + move1[i]][y + move2[i]])
+            if 0 <= x + move1[i] < R and 0 <= y + move2[i] < C and board[x + move1[i]][y + move2[i]] not in root:
+                q.append([x + move1[i], y + move2[i], root + board[x + move1[i]][y + move2[i]]])
+                result = max(result, len(root)+1)
 
 
+solve(0, 0)
 
-
-
-
-
+print(result)
