de523518ce99151f614c577502d786fece222d8e
minjoong <albert507@naver.com>
2020-04-14 00:13:46 +0900

    ..

---
+++ java_algorithm/src/Q_6603.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Q_6603 {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String args[]){
+
+
+
+
+
+
+    }
+
+}
--- java_algorithm/src/practice.java
+++ java_algorithm/src/practice.java
@@ -1,8 +1,8 @@
 public class practice {
     public static void main(String args[]){
 
-        char b = '5';
-        System.out.println();
+        char b = '9';
+        System.out.println((int)b - '0');
 
 
     }
