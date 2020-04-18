8c674b89a87172f924b70b1b37503a225131f823
albert507 <albert507@naver.com>
2020-04-18 19:39:58 +0900

    퇴사 푸는중

---
+++ java_algorithm/src/Q_14501.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Q_14501 {
+    static Scanner sc = new Scanner(System.in);
+    static int N;
+    static int[] day, price;
+    static int benefit = Integer.MIN_VALUE;
+
+    public static void main(String args[]){
+        N = sc.nextInt();
+        day = new int[N];
+        price = new int[N];
+        for(int i = 0; i<N; i++){
+            day[i] = sc.nextInt();
+            price[i] = sc.nextInt();
+        }
+
+
+
+    }
+
+    public static void solve(){
+
+    }
+
+}
