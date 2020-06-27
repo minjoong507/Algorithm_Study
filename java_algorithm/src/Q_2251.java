05903dfc923a565b895b17c33362b025cc090049
minjoong <albert507@naver.com>
2020-06-27 17:49:15 +0900

    물통

--- java_algorithm/src/Q_2251.java
+++ java_algorithm/src/Q_2251.java
@@ -4,46 +4,86 @@ import java.util.Deque;
 import java.util.Scanner;
 import java.util.Collections;
 
+class bottle{
+    int a;
+    int b;
+    int c;
+
+    bottle(int a, int b, int c){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+}
+
 public class Q_2251 {
-    static int volume;
+    static boolean[][][] visited = new boolean[201][201][201];
     static ArrayList<Integer> result = new ArrayList<>();
     static Scanner sc = new Scanner(System.in);
-    static int[] method = new int[4];
+    static int A,B,C;
     public static void main(String args[]){
-        int A = sc.nextInt();
-        int B = sc.nextInt();
-        int C = sc.nextInt();
+        A = sc.nextInt();
+        B = sc.nextInt();
+        C = sc.nextInt();
 
-        method[0] = A;
-        method[1] = -A;
-        method[2] = B;
-        method[3] = -B;
-
-        solve(C);
+        solve(A,B,C);
         Collections.sort(result);
-        for(int val : result) {
-            if (val == 0)
-                continue;
-
+        for(int val : result)
             System.out.print(val + " ");
-        }
     }
 
-
-    public static void solve(int c){
-        Deque<Integer> q = new ArrayDeque<>();
-        q.offer(c);
+    public static void solve(int a, int b, int c){
+        Deque<bottle> q = new ArrayDeque<>();
+        q.offer(new bottle(0,0,c));
 
         while(!q.isEmpty()){
-            volume = q.poll();
-            result.add(volume);
+            bottle cur_bottle = q.poll();
 
-            for(int i = 0; i<method.length; i++){
-                int next = volume + method[i];
-                if(next >= 0 && next <= c && !result.contains(next))
-                    q.offer(next);
-            }
+            if (visited[cur_bottle.a][cur_bottle.b][cur_bottle.c])
+                continue;
 
+
+            visited[cur_bottle.a][cur_bottle.b][cur_bottle.c] = true;
+
+            if (cur_bottle.a == 0 && !result.contains(cur_bottle.c))
+                result.add(cur_bottle.c);
+
+            // 6가지 경우 체크
+            //1. a->b
+            if(cur_bottle.a + cur_bottle.b > B)
+                q.offer(new bottle(cur_bottle.a + cur_bottle.b - B, B, cur_bottle.c));
+            else
+                q.offer(new bottle(0, cur_bottle.a + cur_bottle.b, cur_bottle.c));
+
+            //2. a->c
+            if(cur_bottle.a + cur_bottle.c > C)
+                q.offer(new bottle(cur_bottle.a + cur_bottle.c - C, cur_bottle.b, C));
+            else
+                q.offer(new bottle(0, cur_bottle.b, cur_bottle.a + cur_bottle.c));
+
+            //3. b->a
+            if(cur_bottle.b + cur_bottle.a > A)
+                q.offer(new bottle(A, cur_bottle.b + cur_bottle.a - A, cur_bottle.c));
+            else
+                q.offer(new bottle(cur_bottle.a + cur_bottle.b, 0, cur_bottle.c));
+
+            //4. b->c
+            if(cur_bottle.b + cur_bottle.c > C)
+                q.offer(new bottle(cur_bottle.a, cur_bottle.b + cur_bottle.c - C, C));
+            else
+                q.offer(new bottle(cur_bottle.a, 0, cur_bottle.b + cur_bottle.c));
+
+            //5. c->a
+            if(cur_bottle.c + cur_bottle.a > A)
+                q.offer(new bottle(A, cur_bottle.b, cur_bottle.c + cur_bottle.a - A));
+            else
+                q.offer(new bottle(cur_bottle.a + cur_bottle.c, cur_bottle.b, 0));
+
+            //6. c->b
+            if(cur_bottle.c + cur_bottle.b > B)
+                q.offer(new bottle(cur_bottle.a, B, cur_bottle.c + cur_bottle.b - B));
+            else
+                q.offer(new bottle(cur_bottle.a, cur_bottle.b + cur_bottle.c, 0));
         }
 
     }
