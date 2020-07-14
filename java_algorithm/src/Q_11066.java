0cf0bd886c4130f0b78bdac194578d6828389551
minjoong507 <albert507@naver.com>
2020-07-14 17:58:40 +0900

    푸는중

--- java_algorithm/src/Q_11066.java
+++ java_algorithm/src/Q_11066.java
@@ -1,2 +1,14 @@
+import java.io.*;
+
 public class Q_11066 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args){
+        
+
+
+
+    }
+
+
 }
