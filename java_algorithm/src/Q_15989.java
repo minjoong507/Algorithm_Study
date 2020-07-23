1369cd4e661819348ca972d4f6b3820f02330b0a
minjoong <albert507@naver.com>
2020-07-23 13:21:31 +0900

    1,2,3 더하기 4

--- java_algorithm/src/Q_15989.java
+++ java_algorithm/src/Q_15989.java
@@ -1,33 +1,21 @@
 import java.util.Scanner;
 
 public class Q_15989 {
-    static Scanner sc = new Scanner(System.in);
-    static int Max_val = 10001;
-    static boolean[][][] visited;
-    static int target, result;
-    public static void main(String[] args){
-        int T = sc.nextInt();
-        for(int i = 0; i< T; i++){
-            target = sc.nextInt();
-            result  = 0;
-            visited = new boolean[Max_val][Max_val][Max_val];
-            int[] arr = new int[4];
-            solve(arr, 0);
-            System.out.println(result);
-        }
 
-    }
-    public static void solve(int[] num, int sum){
-        if(sum == target && !visited[num[1]][num[2]][num[3]]){
-            result++;
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int T=sc.nextInt();
+        int[][] dp=new int[10001][4];
+        dp[1][1]=1; dp[2][1]=1; dp[2][2]=1; dp[3][1]=2; dp[3][3]=1;
+        for(int i=4; i<=10000; i++) {
+            dp[i][1]=dp[i-1][1]+dp[i-1][2]+dp[i-1][3];
+            dp[i][2]=dp[i-2][2]+dp[i-2][3];
+            dp[i][3]=dp[i-3][3];
         }
-        else if(sum < target) {
-            for(int i = 1; i <=3; i++){
-                num[i] += 1;
-                solve(num, sum+i);
-            }
+        for(int tc=1; tc<=T; tc++) {
+            int N=sc.nextInt();
+            System.out.print(dp[N][1]+dp[N][2]+dp[N][3]+"\n");
         }
-
     }
 
 }
