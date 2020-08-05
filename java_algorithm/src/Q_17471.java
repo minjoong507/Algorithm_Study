01ef98d97b24874019f3e71b78df4f5be2450578
minjoong <albert507@naver.com>
2020-08-05 20:29:53 +0900

    게리맨더링

---
+++ java_algorithm/src/Q_17471.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Q_17471 {
+    static Scanner sc = new Scanner(System.in);
+    static int N, result = Integer.MAX_VALUE;
+    static int[] popular;
+    static ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
+
+    public static void main(String[] args){
+        N = sc.nextInt();
+        for(int i = 0 ; i < N + 1; i++)
+            arr.add(new ArrayList<>());
+
+        popular = new int[N+1];
+        for(int i = 1; i<N+1; i++)
+            popular[i] = sc.nextInt();
+
+        for(int i = 1; i < N+1; i++){
+            int connect_n = sc.nextInt();
+            for(int j = 0; j < connect_n; j++){
+                int node = sc.nextInt();
+                arr.get(i).add(node);
+                arr.get(node).add(i);
+            }
+        }
+
+    }
+
+    public static void solve(){
+
+
+
+
+
+    }
+
+    // 이렇게 그룹으로 나눌 수 있는지 체크.
+    public static boolean ispossiblegroup(ArrayList<Integer> group){
+
+    }
+
+    // 이렇게 그룹으로 나눴을 때 인구 차이.
+    public static void check_population(ArrayList<Integer> group){
+
+    }
+
+}
