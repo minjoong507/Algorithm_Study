98db5cbde587fd1cf1ea113aa02eb087eaaa9058
minjoong <albert507@naver.com>
2020-08-10 00:11:46 +0900

    푸는중

---
+++ java_algorithm/src/Q_10973.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Q_10973 {
+    static Scanner sc = new Scanner(System.in);
+    static int N;
+
+    public static void main(String[] args){
+        N = sc.nextInt();
+
+
+
+
+
+    }
+
+}
