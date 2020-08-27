98048b4ec3fbcfb4821236ba85d0c66c9fa2b817
MIN JOON <albert507@naver.com>
2020-08-28 00:07:11 +0900

    게리맨더링

--- java_algorithm/src/Q_17471.java
+++ java_algorithm/src/Q_17471.java
@@ -1,49 +1,91 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Q_17471 {
-    static Scanner sc = new Scanner(System.in);
-    static int N, result = Integer.MAX_VALUE;
-    static int[] popular;
-    static ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
+    public static int N, sum, INF = 987654321, min = INF;
+    public static int[] pp, w, tf = {0,1};
+    public static boolean[] v;
+    public static int[][] arr;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-    public static void main(String[] args){
-        N = sc.nextInt();
-        for(int i = 0 ; i < N + 1; i++)
-            arr.add(new ArrayList<>());
+        N = Integer.parseInt(br.readLine());
+        pp = new int[N];
+        w = new int[N];
+        arr = new int[N][N];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i<N; i++) {    //인구수 받기
+            pp[i] = Integer.parseInt(st.nextToken());
+        }
+        for(int i = 0; i<N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int tmp = Integer.parseInt(st.nextToken());    //인접한 구역의 수
+            for(int k = 0; k<tmp; k++) {
+                int j = Integer.parseInt(st.nextToken()) - 1;
+                arr[i][j] = 1;
+            }
+        }
+        permcomb(0);    //중복순열 이용해 파워셋 구현
+        if(min == INF)
+            System.out.println(-1);
+        else
+            System.out.println(min);
+    }
 
-        popular = new int[N+1];
-        for(int i = 1; i<N+1; i++)
-            popular[i] = sc.nextInt();
+    public static void permcomb(int cnt) {
+        if(cnt == N) {
+            v = new boolean[N];
+            int wg1 = 0, wg2 = 0;
+            for(int i = 0; i<N; i++) {
+                if(w[i] == 0 && !v[i]){
+                    System.out.print(i + " ");
+                }
+            }
+            System.out.println("--------");
+                //지역구  1
+            for(int i = 0; i<N; i++) {
+                if(w[i] == 0 && !v[i]) {
+                    sum = 0;
+                    gary(i);
+                    wg1 = sum;
+                    break;
+                }
+            }
 
-        for(int i = 1; i < N+1; i++){
-            int connect_n = sc.nextInt();
-            for(int j = 0; j < connect_n; j++){
-                int node = sc.nextInt();
-                arr.get(i).add(node);
+            //지역구 2
+            for(int i = 0; i<N; i++) {
+                if(w[i] == 1 &&!v[i]) {
+                    sum = 0;
+                    gary(i);
+                    wg2 = sum;
+                    break;
+                }
+            }
+            for(int i = 0; i<N; i++) {
+                if(!v[i])    //두 구역으로 나눠지지 않는 경우 리턴
+                    return;
+            }
+
+            int x = Math.abs(wg1 - wg2);
+            min = min<x?min:x;
+            return;
+        }
+        for(int i = 0; i<tf.length; i++) {
+            w[cnt] = tf[i];
+            permcomb(cnt+1);
+        }
+    }
+    public static void gary(int now) {
+        v[now] = true;
+        sum += pp[now];
+        for(int i = 0; i<N; i++) {
+            if(!v[i] && w[i] == w[now] && arr[i][now] == 1) {
+                gary(i);
             }
         }
 
     }
-
-    public static void solve(){
-//        for(int i = 1; i< N+1; i++){
-//
-//        }
-
-
-
-
-    }
-
-    // 이렇게 그룹으로 나눌 수 있는지 체크.
-    public static boolean ispossiblegroup(ArrayList<Integer> group){
-        return false;
-    }
-
-    // 이렇게 그룹으로 나눴을 때 인구 차이.
-    public static void check_population(ArrayList<Integer> group){
-
-    }
-
 }
--- java_algorithm/src/practice.java
+++ java_algorithm/src/practice.java
@@ -5,14 +5,30 @@ import java.util.Deque;
 public class practice {
 
     static ArrayList<Integer> bag = new ArrayList<>();
-
+    static int[] check = new int[6];
+    static int[] mark = {0, 1};
     public static void main(String args[]){
-        bag.add(1);
-        bag.add(7);
-        bag.add(7);
-        bag.add(7);
-        bag.add(2);
-        System.out.println(bag.indexOf(2));
+        dfs(0);
+    }
+
+    public static void dfs(int cnt){
+        if(cnt == 6){
+            for(int i=0; i< check.length; i++){
+                if(check[i] == 0){
+                    System.out.print(i + " ");
+                }
+            }
+            System.out.println("-------");
+            return;
+        }
+
+        for(int k = 0; k < 2; k++){
+            check[cnt] = mark[k];
+            dfs(cnt + 1);
+        }
+
+
+
     }
 
 
