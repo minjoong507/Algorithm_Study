18bbbd529bbcc7dbcd6da9d6f7948b343a37c4ee
minjoong <albert507@naver.com>
2020-10-14 12:47:49 +0900

    버블 정렬

--- java_algorithm/src/BubbleSort.java
+++ java_algorithm/src/BubbleSort.java
@@ -1,7 +1,20 @@
 public class BubbleSort {
     public static void main(String[] args){
         int[] a = new int[]{1, 8, 9, 7, 5, 6, 2, 3, 4};
+        int tmp = 0;
+        for(int i = 0; i < a.length; i++){
+            for(int j = 0; j < a.length - i - 1; j++){
+                if(a[j] > a[j+1]){
+                    tmp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = tmp;
+                }
+            }
+        }
 
+        for(int k: a){
+            System.out.print(k + " ");
+        }
 
 
     }
